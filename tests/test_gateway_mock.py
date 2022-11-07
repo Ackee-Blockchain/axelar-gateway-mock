a06c02135006304414b7f0329d63b38902fd1ecc
Michal Převrátil <michprev@gmail.com>
2022-11-07 18:59:24 +0100

    🚧 Add more assertions to gateway mock test

--- tests/test_gateway_mock.py
+++ tests/test_gateway_mock.py
@@ -20,14 +20,21 @@ def test_send_token():
 
     user1 = Address("0x4516d3f5b2e5b6e1f062f18e3d2f5f6a77469285")
     user1.balance = Wei.from_ether(100)
+    assert user1.balance == Wei.from_ether(100)
     user2 = Address("0x781d3f5b2e5b6e1f062f18e3d2f5f6a774692851")
 
     token1.mint(user1, 100)
+
     assert token1.balanceOf(user1) == 100
+    assert token1.balanceOf(user2) == 0
+    assert token2.balanceOf(user1) == 0
+    assert token2.balanceOf(user2) == 0
+
     token1.approve(gateway1, 100, from_=user1)
 
     gateway1.sendToken("chain2", str(user2), "TKN", 100, from_=user1)
 
+    assert token1.balanceOf(user1) == 0
     assert token2.balanceOf(user2) == 100
 
 
@@ -41,6 +48,7 @@ def test_call_contract():
 
     contract1 = ExampleContract.deploy(gateway1)
     Address(contract1).balance = Wei.from_ether(100)
+    assert Address(contract1).balance == Wei.from_ether(100)
     contract2 = ExampleContract.deploy(gateway2)
 
     contract1.send("chain2", str(Address(contract2)), b"testing message")
@@ -65,13 +73,19 @@ def test_call_contract_with_token():
 
     contract1 = ExampleContract.deploy(gateway1)
     Address(contract1).balance = Wei.from_ether(100)
+    assert Address(contract1).balance == Wei.from_ether(100)
     contract2 = ExampleContract.deploy(gateway2)
 
     token1.mint(contract1, 200)
+
     assert token1.balanceOf(contract1) == 200
+    assert token1.balanceOf(contract2) == 0
+    assert token2.balanceOf(contract1) == 0
+    assert token2.balanceOf(contract2) == 0
 
     contract1.sendWithToken("chain2", str(Address(contract2)), b"testing message", token1, "TKN", 200)
 
     assert contract2.lastMessage() == ExampleContract.Message(
         "chain1", str(Address(contract1)).lower(), b"testing message", "TKN", 200)
+    assert token1.balanceOf(contract1) == 0
     assert token2.balanceOf(contract2) == 200
